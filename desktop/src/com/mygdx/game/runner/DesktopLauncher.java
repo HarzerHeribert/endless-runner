4b3c112659fcaa142281f9e7e571cc4b6a2353b9
HarzerHeribert <eneasdembach@gmail.com>
2024-05-28 18:35:12 +0200

    removed runner game

--- desktop/src/com/mygdx/game/runner/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/runner/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.mygdx.game.runner;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.runner.RunnerGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
